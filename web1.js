7d2f8be6e238a2e210ad79c075f85bcb4a63ed10
StellaR0511 <81784995+StellaR0511@users.noreply.github.com>
2022-10-21 14:14:58 -0500

    Add files via upload

---
+++ web1.js
@@ -0,0 +1,51 @@
+img = "";
+status = "";
+object = [];
+
+function setup(){
+canvas = createCanvas(640,420);
+canvas.center();
+objectDetected = ml5.objectDetector("cocossd", modelLoaded);
+document.getElementById("status").innerHTML = "Status: Detecting Objects";
+
+}
+
+function modelLoaded(){
+    console.log("Model Loaded!");
+    status = true;
+    objectDetected.detect(img, gotResult);
+}
+
+function gotResult(error, results){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        object = results;
+    }
+}
+
+function preload(){
+
+img = loadImage("web1.jpg");
+
+}
+
+function draw(){
+
+image(img, 0,0,640,420);
+if(status!=""){
+    document.getElementById("status").innerHTML = "Status: Object Detected";
+    for(i=0;i<object.length;i++){
+        //console.log(object);
+        fill("red");
+        percent = floor(object[i].confidence*100);
+        text(object[i].label+" "+percent+"%", object[i].x+5,object[i].y+20);
+        noFill();
+        stroke("red");
+        rect(object[i].x,object[i].y, object[i].width, object[i].height);
+    }
+}
+
+}
